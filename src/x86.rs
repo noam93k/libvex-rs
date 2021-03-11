6152800556f30e92ece4dfa42a9cd6174ed17a76
Noam Shalom Kleinburd <noam93k@gmail.com>
2021-03-11 20:45:34 +0200

    Put x86 specific constants in submodules.

--- src/x86.rs
+++ src/x86.rs
@@ -1,27 +1,3 @@
-pub use vex_sys::{
-    OFFSET_x86_EAX,
-    OFFSET_x86_EBX,
-    OFFSET_x86_ECX,
-    OFFSET_x86_EDX,
-    OFFSET_x86_ESI,
-    OFFSET_x86_EDI,
-    OFFSET_x86_EBP,
-    OFFSET_x86_ESP,
-    OFFSET_x86_EIP,
-    OFFSET_x86_CS,
-    OFFSET_x86_DS,
-    OFFSET_x86_ES,
-    OFFSET_x86_FS,
-    OFFSET_x86_GS,
-    OFFSET_x86_SS,
-
-    VEX_HWCAPS_X86_MMXEXT,
-    VEX_HWCAPS_X86_SSE1,
-    VEX_HWCAPS_X86_SSE2,
-    VEX_HWCAPS_X86_SSE3,
-    VEX_HWCAPS_X86_LZCNT,
-};
-
 pub struct State(pub vex_sys::VexGuestX86State);
 
 impl Default for State {
@@ -32,4 +8,34 @@ impl Default for State {
             this.assume_init()
         })
     }
+}
+
+pub mod offset {
+    use vex_sys::Int;
+
+    pub const EAX: Int = vex_sys::OFFSET_x86_EAX as Int;
+    pub const EBX: Int = vex_sys::OFFSET_x86_EBX as Int;
+    pub const ECX: Int = vex_sys::OFFSET_x86_ECX as Int;
+    pub const EDX: Int = vex_sys::OFFSET_x86_EDX as Int;
+    pub const ESI: Int = vex_sys::OFFSET_x86_ESI as Int;
+    pub const EDI: Int = vex_sys::OFFSET_x86_EDI as Int;
+    pub const EBP: Int = vex_sys::OFFSET_x86_EBP as Int;
+    pub const ESP: Int = vex_sys::OFFSET_x86_ESP as Int;
+    pub const EIP: Int = vex_sys::OFFSET_x86_EIP as Int;
+    pub const CS: Int = vex_sys::OFFSET_x86_CS as Int;
+    pub const DS: Int = vex_sys::OFFSET_x86_DS as Int;
+    pub const ES: Int = vex_sys::OFFSET_x86_ES as Int;
+    pub const FS: Int = vex_sys::OFFSET_x86_FS as Int;
+    pub const GS: Int = vex_sys::OFFSET_x86_GS as Int;
+    pub const SS: Int = vex_sys::OFFSET_x86_SS as Int;
+}
+
+pub mod hw_caps {
+    pub use vex_sys::{
+        VEX_HWCAPS_X86_MMXEXT as MMXEXT,
+        VEX_HWCAPS_X86_SSE1 as SSE1,
+        VEX_HWCAPS_X86_SSE2 as SSE2,
+        VEX_HWCAPS_X86_SSE3 as SSE3,
+        VEX_HWCAPS_X86_LZCNT as LZCNT,
+    };
 }
